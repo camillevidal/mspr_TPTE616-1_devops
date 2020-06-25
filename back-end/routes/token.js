cfe50a0750087d50b31753c941ffbef641a4d265
romain <romain-hd.perez@live.fr>
2020-06-25 19:04:51 +0200

    modify update token

--- back-end/routes/token.js
+++ back-end/routes/token.js
@@ -28,16 +28,16 @@ router.get('/token/:id', (req, res) => {
     console.log(temp_token)
     if(req.params.id.indexOf("ip_code") !== -1){
         let temp_ip = temp[1].replace("ip_code=", "");
-        sql_update = 'UPDATE Connection Set token = null and lastip =' + temp_ip + '" WHERE token ="' + temp_token +'"';
+        sql_update = 'UPDATE Connection Set token = null and lastip ="' + temp_ip + '" WHERE token ="' + temp_token +'";';
 
     }
     if(req.params.id.indexOf("browser") !== -1){
         let temp_browser = temp[1].replace("browser=", "");
-        sql_update = 'UPDATE Connection Set token = null and lastbrowser =' + temp_browser + '" WHERE token ="' + temp_token +'"';
+        sql_update = 'UPDATE Connection Set token = null and lastbrowser ="' + temp_browser + '" WHERE token ="' + temp_token +'";';
     }
 
     console.log(sql_update)
-    let sql_insert = 'Select id from Connection WHERE token =' +'"' + temp_token +'"';
+    let sql_insert = 'Select id from Connection WHERE token ="' + temp_token +'";';
 
     // execute the insert statment
     co_insert.query(sql_insert, function (err, result, fields) {
