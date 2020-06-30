0eb7077dc30c0e5ac10136c6546dde68f810f395
romain <romain-hd.perez@live.fr>
2020-06-30 21:11:16 +0200

    modify login-service.service.ts

--- front-end/src/app/services/login-service/login-service.service.ts
+++ front-end/src/app/services/login-service/login-service.service.ts
@@ -15,57 +15,6 @@ export class LoginServiceService {
   constructor(private _http: HttpClient) {
   }
 
-  // loginAuth(userObj: any) {
-  //   if (userObj.authcode) {
-  //     console.log('Appending headers');
-  //     this.headerOptions = new HttpHeaders({
-  //       'x-tfa': userObj.authcode
-  //     });
-  //   }
-  //   console.log(userObj.uip)
-  //  return this._http.post("http://portail.chatelet.dutmen.fr:3000/login", { uname: userObj.uname, upass: userObj.upass, uip: userObj.uip, ubrowser: userObj.ubrowser }, { observe: 'response', headers: this.headerOptions });
-  // }
-  //
-  // setupAuth() {
-  //   return this._http.post("http://portail.chatelet.dutmen.fr:3000/tfa/setup", {}, { observe: 'response' })
-  // }
-  //
-  // registerUser(userObj: any) {
-  //   console.log("user object "+JSON.stringify(userObj))
-  //   return this._http.post("http://portail.chatelet.dutmen.fr:3000/register", { uname: userObj.uname, upass: userObj.upass,uip:userObj.uip , ubrowser:userObj.ubrowser}, { observe: 'response' });
-  // }
-  //
-  // updateAuthStatus(value: boolean) {
-  //   this._isLoggedIn = value
-  //   this.authSub.next(this._isLoggedIn);
-  //   localStorage.setItem('isLoggedIn', value ? "true" : "false");
-  // }
-  //
-  // getAuthStatus() {
-  //   this._isLoggedIn = localStorage.getItem('isLoggedIn') == "true" ? true : false;
-  //   return this._isLoggedIn
-  // }
-  //
-  // logoutUser() {
-  //   this._isLoggedIn = false;
-  //   this.authSub.next(this._isLoggedIn);
-  //   localStorage.setItem('isLoggedIn', "false")
-  // }
-  //
-  // getAuth() {
-  //   return this._http.get("http://portail.chatelet.dutmen.fr:3000/tfa/setup", { observe: 'response' });
-  // }
-  // getUser(user:String){
-  //   return this._http.get(`http://portail.chatelet.dutmen.fr:3000/user/${user}`)
-  // }
-  // deleteAuth() {
-  //   return this._http.delete("http://portail.chatelet.dutmen.fr:3000/tfa/setup", { observe: 'response' });
-  // }
-  //
-  // verifyAuth(token: any) {
-  //   return this._http.post("http://portail.chatelet.dutmen.fr:3000/tfa/verify", { token }, { observe: 'response' });
-  // }
-
   loginAuth(userObj: any) {
     if (userObj.authcode) {
       console.log('Appending headers');
@@ -74,16 +23,16 @@ export class LoginServiceService {
       });
     }
     console.log(userObj.uip)
-    return this._http.post("http://localhost:3000/login", { uname: userObj.uname, upass: userObj.upass, uip: userObj.uip, ubrowser: userObj.ubrowser }, { observe: 'response', headers: this.headerOptions });
+   return this._http.post("http://portail.chatelet.dutmen.fr:3000/login", { uname: userObj.uname, upass: userObj.upass, uip: userObj.uip, ubrowser: userObj.ubrowser }, { observe: 'response', headers: this.headerOptions });
   }
 
   setupAuth() {
-    return this._http.post("http://localhost:3000/tfa/setup", {}, { observe: 'response' })
+    return this._http.post("http://portail.chatelet.dutmen.fr:3000/tfa/setup", {}, { observe: 'response' })
   }
 
   registerUser(userObj: any) {
     console.log("user object "+JSON.stringify(userObj))
-    return this._http.post("http://localhost:3000/register", { uname: userObj.uname, upass: userObj.upass,uip:userObj.uip , ubrowser:userObj.ubrowser}, { observe: 'response' });
+    return this._http.post("http://portail.chatelet.dutmen.fr:3000/register", { uname: userObj.uname, upass: userObj.upass,uip:userObj.uip , ubrowser:userObj.ubrowser}, { observe: 'response' });
   }
 
   updateAuthStatus(value: boolean) {
@@ -104,16 +53,17 @@ export class LoginServiceService {
   }
 
   getAuth() {
-    return this._http.get("http://localhost:3000/tfa/setup", { observe: 'response' });
+    return this._http.get("http://portail.chatelet.dutmen.fr:3000/tfa/setup", { observe: 'response' });
   }
   getUser(user:String){
-    return this._http.get(`http://localhost:3000/user/${user}`)
+    return this._http.get(`http://portail.chatelet.dutmen.fr:3000/user/${user}`)
   }
   deleteAuth() {
-    return this._http.delete("http://localhost:3000/tfa/setup", { observe: 'response' });
+    return this._http.delete("http://portail.chatelet.dutmen.fr:3000/tfa/setup", { observe: 'response' });
   }
 
   verifyAuth(token: any) {
-    return this._http.post("http://localhost:3000/tfa/verify", { token }, { observe: 'response' });
+    return this._http.post("http://portail.chatelet.dutmen.fr:3000/tfa/verify", { token }, { observe: 'response' });
   }
+
 }
